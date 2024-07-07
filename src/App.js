cbc3890dfdfcb8b63bf8845e8dda8e2ab335c5a3
abhigun1234 <37846168+abhigun1234@users.noreply.github.com>
2024-07-07 18:02:05 +0530

    add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Registration from './Registration';
 import Login from './Login';
 import User from './User';
+import Header from './Header';
+import { BrowserRouter, Route,Routes} from 'react-router-dom';
+import FormComp from './FormComp';
 
 function App() {
   return (
@@ -10,7 +13,16 @@ function App() {
       {/* <Registration></Registration> */}
       {/* <Login></Login> */}
      {/* <User></User> */}
-     <Registration></Registration>
+     {/* <Registration></Registration> */}
+     <BrowserRouter>
+     <Header></Header>
+     <Routes>
+     <Route path="forms" element={<FormComp />} ></Route>
+     <Route path="registration" element={<Registration></Registration>}></Route>
+
+     </Routes>
+     </BrowserRouter>
+  
     </div>
   );
 }
---
+++ src/FormComp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function FormComp(props) {
+    return (
+        <div>
+            <ul>
+                <li>Class based component</li>
+                <li>Functional based component</li>
+            </ul>
+        </div>
+    );
+}
+
+export default FormComp;
